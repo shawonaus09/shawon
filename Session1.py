105e547966926a520d945d8e96adcca410215a65
shawonaus09 <230940@students.aih.nsw.edu.au>
2024-05-27 18:38:31 +1000

    Create Session1.py

---
+++ Session1.py
@@ -0,0 +1,22 @@
+# write a program to input age of a user, and let them know if they have diabete:
+# risk or not. use case is that a user with age more than 65 is risky else not 
+# risky
+
+Age = 55
+if age >= 65
+ print("This is risky")
+else 
+print ("Not risky")
+
+# write a program to calculate the GST tax of two products,apple and pie 
+# apple has 10% GST and pie has only 8%
+# Calculate total GST that need to be deducted from the bill
+# orange gross sale is $75,pie gross is $100
+
+apple = 75
+pie = 100
+GST_apple = 0.1*apple 
+GST_pie = 0.08*pie
+
+print ("GST_apple")
+print ("GST_pie")
