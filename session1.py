46e1ba29b52273982f2729f18462d6ad66587888
shawonaus09 <230940@students.aih.nsw.edu.au>
2024-05-27 20:35:29 +1000

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,15 @@
+# write a program to calculate the number of minis in the maxis,
+# total maxis is 2.5*minis
+# 1 mini is 5000
+# calculate how much is 4.5 maxis 
+
+minis is 500
+maxis is 2.5*minis
+ so 4.5 maxis is equal to 2.5*4.5*minis
+Display maxis
+
+minis = 500
+maxis = 2.5*minis
+print ("the value for maxis is: ",4.5*maxis)
+
+
