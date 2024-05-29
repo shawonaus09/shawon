ac8e09042a00f8f336b7206ff281ddd6ec94cf6b
shawonaus09 <230940@students.aih.nsw.edu.au>
2024-05-29 20:27:38 +1000

    Create session1(B).py

---
+++ session1(B).py
@@ -0,0 +1,9 @@
+import statistics #import your modules
+# initializing list # this is a list
+li=[1,2,3,4,2,2,2,1] # lis is the variables in the list
+
+# using mean() to calculate average of list
+# elements
+print("The average of list values is : ",end="") # print statements
+print(statistics.mean(li))# print the value of mean)
+print(statistics.median(li))
